aeb96897aa969a4d7998f16c14596ee7ad6a1041
Nikunj Singh <nikunj.singh@live.com>
2025-08-22 22:08:30 -0400

    Initial Commit

---
+++ glass photo viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  glass photo viewer
+//
+//  Created by Nikunj Singh on 8/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ glass photo viewer/glass_photo_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  glass_photo_viewerApp.swift
+//  glass photo viewer
+//
+//  Created by Nikunj Singh on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct glass_photo_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
